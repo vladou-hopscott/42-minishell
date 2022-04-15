182e9c318a93570bc074181b3414c10ae4b2258a
hopscott <scott.willis2706@gmail.com>
2022-04-15 21:34:59 +0200

    echo and pwd working nicely - check perror success print issue + changed CWD key to PWD as in typical env

--- lib/builtins/include/builtins.h
+++ lib/builtins/include/builtins.h
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 15:50:12 by swillis           #+#    #+#             */
-/*   Updated: 2022/04/15 20:03:33 by swillis          ###   ########.fr       */
+/*   Updated: 2022/04/15 21:25:27 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,26 @@
 # define BUILTINS_H
 
 # include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+//********** FILES **********//
+char	**ft_freetbl(char **tbl, int pos);
+char	**ft_split(char *s, char c);
+char	*ft_strdup(char *src);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strjoin(char *s1, char *s2);
 
 //********** UTILS **********//
 size_t	ft_strlen(char *str);
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 
-//********** ECHO **********//
+//********** UTILS_ENV **********//
+int		ft_tbllen(char **tbl);
+char	**tbl_append(char **env, char *str);
+int		env_findkeypos(char *key, char **env);
+char	*env_findkeyvalue(char *key, char **env);
+char	**env_export(char *key, char *value, char **env);
 
 #endif
--- lib/builtins/src/ft_strjoin.c
+++ lib/builtins/src/ft_strjoin.c
@@ -1,28 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/15 20:02:28 by swillis           #+#    #+#             */
-/*   Updated: 2022/04/15 20:03:55 by swillis          ###   ########.fr       */
+/*   Created: 2021/11/24 23:30:19 by swillis           #+#    #+#             */
+/*   Updated: 2022/04/15 21:25:16 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int	main(int ac, char **av)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	int	i;
+	char	*str;
+	int		len_s1;
+	int		len_s2;
+	int		i;
+	int		j;
 
-	i = 1;
-	while (i < ac)
-	{
-		ft_putstr(av[i]);
-		if (i < ac - 1)
-			ft_putchar(' ');
-		i++;
-	}
-	ft_putchar('\n');
+	if (!s1 || !s2)
+		return (0);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
 }
