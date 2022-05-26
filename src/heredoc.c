f937c38918236bbe1a34a7e87432c4d4fcc3ce1f
Vladimir NAFISSI <vnafissi@e1r5p3.clusters.42paris.fr>
2022-05-26 14:37:50 +0200

    clean \n character in heredoc file

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: vnafissi <vnafissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:45:38 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/05/26 13:57:37 by vnafissi         ###   ########.fr       */
+/*   Updated: 2022/05/26 14:36:09 by vnafissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 int	heredoc(char *delimitor, t_cmd_line ** cmd_line)
 {
 	char	*tmp;
+	int		i;
 
 	(*cmd_line)->heredoc_mode = 1;
 	(*cmd_line)->fdin = open("./heredoc", O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	i = 0;
 	while (1)
 	{
 		tmp = readline("> ");
@@ -27,9 +29,11 @@ int	heredoc(char *delimitor, t_cmd_line ** cmd_line)
 			ft_free_null_str(&tmp);
 			break;
 		}
-		ft_putstr_fd("\n", (*cmd_line)->fdin);
+		if (i > 0)
+			ft_putstr_fd("\n", (*cmd_line)->fdin);
 		ft_putstr_fd(tmp, (*cmd_line)->fdin);
 		ft_free_null_str(&tmp);
+		i++;
 	}
 	return (0);
 }
