ab33f9c1102b3108b72be809c4fbd5aab19db4e1
Vladimir NAFISSI <vnafissi@e1r5p3.clusters.42paris.fr>
2022-05-26 13:58:37 +0200

    amelioration gestion erreurs + free readline for heredoc

--- src/heredoc.c
+++ src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: vnafissi <vnafissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:45:38 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/05/26 12:05:47 by vnafissi         ###   ########.fr       */
+/*   Updated: 2022/05/26 13:57:37 by vnafissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ int	heredoc(char *delimitor, t_cmd_line ** cmd_line)
 	{
 		tmp = readline("> ");
 		if (!ft_strncmp(tmp, delimitor, ft_strlen(delimitor)) && ft_strlen(tmp) == ft_strlen(delimitor))
+		{
+			ft_free_null_str(&tmp);
 			break;
+		}
 		ft_putstr_fd("\n", (*cmd_line)->fdin);
 		ft_putstr_fd(tmp, (*cmd_line)->fdin);
+		ft_free_null_str(&tmp);
 	}
 	return (0);
 }
-
-//close((*cmd_line)->fdin);
