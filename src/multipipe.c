bf8bd6c4f9b517d087b45c46af885129d2887c7a
hopscott <scott.willis2706@gmail.com>
2022-06-28 15:10:12 +0200

    fixed close of stdin in multipipe

--- src/multipipe.c
+++ src/multipipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   multipipe.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vnafissi <vnafissi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 12:12:24 by vnafissi          #+#    #+#             */
-/*   Updated: 2022/06/28 12:31:01 by vnafissi         ###   ########.fr       */
+/*   Updated: 2022/06/28 13:55:29 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 
 extern t_sh	g_sh;
 
+void	close_fds(int fd1, int fd2)
+{
+	if (fd1 != STDIN_FILENO)
+		close(fd1);
+	if (fd2 != STDOUT_FILENO)
+		close(fd2);
+}
+
 int	spawn_process(int fdin, int fdout, t_cmd_line *cmdl, t_sh *sh)
 {
-	pid_t pid;
+	pid_t	pid;
 
 	pid = fork();
 	if (pid < 0)
@@ -36,13 +44,8 @@ int	spawn_process(int fdin, int fdout, t_cmd_line *cmdl, t_sh *sh)
 		executor(cmdl, &sh->env);
 		return (0);
 	}
+	close_fds(fdin, fdout);
 	return (pid);
-}
-
-void	close_fds(int fd1, int fd2)
-{
-	close(fd1);
-	close(fd2);
 }
 
 void	execute_pipes(t_sh *sh)
@@ -63,6 +66,8 @@ void	execute_pipes(t_sh *sh)
 		cmdl = cmdl->next;
 	}
 	cmdl->pid = spawn_process(fdin, cmdl->fdout, cmdl, sh);
+	if (fdin != STDIN_FILENO)
+		close(fdin);
 	if ((sh->cmd_line_lst)->next != NULL)
 		close_fds(fd[1], fd[0]);
 	cmdl = sh->cmd_line_lst;
